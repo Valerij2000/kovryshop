fffc8eaf52f1bf6985e94073f0e31659d21c934c
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-01-09 20:52:15 +0400

    create catalog and template forms liner regs

---
+++ liner-prod-list.php
@@ -0,0 +1,129 @@
+<div id="list-view" class="tab-pane fade" role="tabpanel">
+    <div class="cat-list-view">
+        <div class="hvr2 row">
+            <div class="portfolio-wrapper">
+                <div class="col-md-3 col-sm-5">
+                    <div class="portfolio-thumb">
+                        <img src="assets/img/liner-catalog/<?php echo mt_rand(1, 5); ?>.jpg" alt="">
+                        <div class="portfolio-content">
+                            <div class="pop-up-icon">
+                                <a class="center-link" href="#product-preview" data-toggle="modal"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9 col-sm-7">
+                    <div class="product-content">
+                        <a class="title-3 fsz-16" href="#"> Ковролин Omphale </a>
+                        <div class="rating">
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                        </div>
+                        <p class="font-3">На данный товар действуют скидки. <a href="contact-us.php"><span style="white-space: nowrap;">Написать консультанту</span></a></span> </p>
+                        <p class="font-3"> В наличии<span class="grn-clr"> Есть </span> </p>
+                        <p> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hvr2 row">
+            <div class="portfolio-wrapper">
+                <div class="col-md-3 col-sm-5">
+                    <div class="portfolio-thumb">
+                        <img src="assets/img/liner-catalog/<?php echo mt_rand(6, 10); ?>.jpg" alt="">
+                        <div class="portfolio-content">
+                            <div class="pop-up-icon">
+                                <a class="center-link" href="#product-preview" data-toggle="modal"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9 col-sm-7">
+                    <div class="product-content">
+                        <a class="title-3 fsz-16" href="#"> Ковролин Masquerade </a>
+                        <div class="rating">
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                        </div>
+                        <p class="font-3">На данный товар действуют скидки. <a href="contact-us.php"><span style="white-space: nowrap;">Написать консультанту</span></a></span> </p>
+                        <p class="font-3"> В наличии<span class="red-clr"> Нет </span> </p>
+                        <p> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hvr2 row">
+            <div class="portfolio-wrapper">
+                <div class="col-md-3 col-sm-5">
+                    <div class="portfolio-thumb">
+                        <img src="assets/img/liner-catalog/<?php echo mt_rand(11, 15); ?>.jpg" alt="">
+                        <div class="portfolio-content">
+                            <div class="pop-up-icon">
+                                <a class="center-link" href="#product-preview" data-toggle="modal"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9 col-sm-7">
+                    <div class="product-content">
+                        <a class="title-3 fsz-16" href="#"> Ковролин Equinox</a>
+                        <div class="rating">
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star half"></span>
+                        </div>
+                        <p class="font-3">На данный товар действуют скидки. <a href="contact-us.php"><span style="white-space: nowrap;">Написать консультанту</span></a></span> </p>
+                        <p class="font-3"> В наличии<span class="grn-clr"> Есть </span> </p>
+                        <p> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="hvr2 row">
+            <div class="portfolio-wrapper">
+                <div class="col-md-3 col-sm-5">
+                    <div class="portfolio-thumb">
+                        <img src="assets/img/liner-catalog/<?php echo mt_rand(16, 24); ?>.jpg" alt="">
+                        <div class="portfolio-content">
+                            <div class="pop-up-icon">
+                                <a class="center-link" href="#product-preview" data-toggle="modal"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9 col-sm-7">
+                    <div class="product-content">
+                        <a class="title-3 fsz-16" href="#"> Ковролин Revelation </a>
+                        <div class="rating">
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                            <span class="star active"></span>
+                        </div>
+                        <p class="font-3">На данный товар действуют скидки. <a href="contact-us.php"><span style="white-space: nowrap;">Написать консультанту</span></a></span> </p>
+                        <p class="font-3"> В наличии<span class="grn-clr"> Есть </span> </p>
+                        <p> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="woocommerce-pagination">
+        <ul class="page-numbers">
+            <!-- <li><a class="next page-numbers" href="#"> <i class="fa fa-angle-left"></i> </a></li> -->
+            <li><span class="page-numbers current">1</span></li>
+            <!-- <li><a class="page-numbers" href="#">2</a></li> -->
+            <!--  <li><a class="next page-numbers" href="#"> <i class="fa fa-angle-right"></i> </a></li> -->
+        </ul>
+    </nav>
+</div>
