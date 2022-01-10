48363502337b5af9941c7d3587d60a5f8ec6e854
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-01-10 17:06:37 +0400

    move search popup in views

--- contact-us.php
+++ contact-us.php
@@ -124,17 +124,8 @@
         <!-- FOOTER -->
           <?php include ('footer.php'); ?>
 
-        <!-- Search Popup -->
-        <div class="popup-box page-search-box">
-            <div>
-                <div class="popup-box-inner">
-                    <form>
-                        <input class="search-query" type="text" placeholder="Search and hit enter" />
-                    </form>
-                </div>
-            </div>
-            <a href="javascript:void(0)" class="close-popup-box close-page-search"><i class="fa fa-close"></i></a>
-        </div>
+       <!-- Search Popup -->
+       <?php include ('search-popup.php'); ?>
         <!-- / Search Popup -->
 
         <!-- Popup: Login 1 -->
---
+++ search-popup.php
@@ -0,0 +1,12 @@
+  <!-- Search Popup -->
+  <div class="popup-box page-search-box">
+      <div>
+          <div class="popup-box-inner">
+              <form>
+                  <input class="search-query" type="text" placeholder="Найти" />
+              </form>
+          </div>
+      </div>
+      <a href="javascript:void(0)" class="close-popup-box close-page-search"><i class="fa fa-close"></i></a>
+  </div>
+  <!-- / Search Popup -->
