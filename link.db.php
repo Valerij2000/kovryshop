ebe93d7bc08ce3b53277a3db08a49f0d8d2448d5
Валерий Шумков <valerijsumkov@MacBook-Pro-Valerij.local>
2022-04-14 00:58:07 +0400

    create bg comment

--- link.db.php
+++ link.db.php
@@ -1,8 +1,23 @@
 <?php 
+
+// localhost
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'root');
+// define('DB_NAME', 'mysql');
+
+// $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// if ($mysql->connect_errno) exit('Error connected!');
+// $mysql->set_charset('utf8');
+
+// hosting
+
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'mysql');
+define('DB_USER', 'valer416_kovry');
+define('DB_PASSWORD', 'alina1606');
+define('DB_NAME', 'valer416_kovryshop');
 
 $mysql = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
